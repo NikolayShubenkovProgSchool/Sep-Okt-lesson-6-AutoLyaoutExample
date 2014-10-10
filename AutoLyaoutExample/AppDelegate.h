a1f77da84535599bb5a5949a3225ef266a78315f
Nikolay Shubenkov <n.shubenkov@gmail.com>
2014-10-10 20:40:59 +0400

    Initial Commit

---
+++ AutoLyaoutExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLyaoutExample
+//
+//  Created by n.shubenkov on 10/10/14.
+//  Copyright (c) 2014 n.shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLyaoutExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoLyaoutExample
+//
+//  Created by n.shubenkov on 10/10/14.
+//  Copyright (c) 2014 n.shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
